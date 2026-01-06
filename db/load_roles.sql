9c9bb4e765ff49f4846e1e59b87516ce068c9e46
OJOMB <oscarm-b@tutamail.com>
2026-01-06 14:46:31 +0100

    fix user load and include proper UUID7s

--- db/load_roles.sql
+++ db/load_roles.sql
@@ -1,27 +1,27 @@
 INSERT INTO roles (id, name, description, created_at, created_by, updated_at, updated_by) VALUES
-    (gen_random_uuid(), 'admin', 'Administrator with full access', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'user', 'Regular user with limited access', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa');
+    (gen_random_uuid(), 'admin', 'Administrator with full access', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'user', 'Regular user with limited access', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98');
 
 INSERT INTO permissions (id, name, version, resource, operation, description, created_at, created_by, updated_at, updated_by) VALUES
-    (gen_random_uuid(), 'create', 'v1', 'users', 'post', 'Permission to create a new user', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'delete', 'v1', 'users', 'delete', 'Permission to delete a user', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'update', 'v1', 'users', 'patch', 'Permission to update user information', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'get', 'v1', 'users', 'get', 'Permission to view user information', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'list', 'v1', 'users', 'get', 'Permission to list users', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'list_followers', 'v1', 'users', 'get', 'Permission to list followers', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'list_followees', 'v1', 'users', 'get', 'Permission to list followees', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'follow', 'v1', 'users', 'put', 'Permission to follow another user', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'unfollow', 'v1', 'users', 'delete', 'Permission to unfollow another user', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'generate_presigned_url', 'v1', 'users', 'post', 'Permission to create presigned url for user profile picture upload', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
+    (gen_random_uuid(), 'create', 'v1', 'users', 'post', 'Permission to create a new user', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'delete', 'v1', 'users', 'delete', 'Permission to delete a user', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'update', 'v1', 'users', 'patch', 'Permission to update user information', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'get', 'v1', 'users', 'get', 'Permission to view user information', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'list', 'v1', 'users', 'get', 'Permission to list users', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'list_followers', 'v1', 'users', 'get', 'Permission to list followers', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'list_followees', 'v1', 'users', 'get', 'Permission to list followees', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'follow', 'v1', 'users', 'put', 'Permission to follow another user', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'unfollow', 'v1', 'users', 'delete', 'Permission to unfollow another user', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'generate_presigned_url', 'v1', 'users', 'post', 'Permission to create presigned url for user profile picture upload', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
 
-    (gen_random_uuid(), 'create', 'v1', 'fighters', 'post', 'Permission to create a new fighter', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'delete', 'v1', 'fighters', 'delete', 'Permission to delete a fighter', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'update', 'v1', 'fighters', 'put', 'Permission to update fighter information', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    (gen_random_uuid(), 'get', 'v1', 'fighters', 'get', 'Permission to view fighter information', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa');
+    (gen_random_uuid(), 'create', 'v1', 'fighters', 'post', 'Permission to create a new fighter', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'delete', 'v1', 'fighters', 'delete', 'Permission to delete a fighter', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'update', 'v1', 'fighters', 'put', 'Permission to update fighter information', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'),
+    (gen_random_uuid(), 'get', 'v1', 'fighters', 'get', 'Permission to view fighter information', now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98');
 
 -- Assign all above permissions to admin and user role
 INSERT INTO role_permissions (role_id, permission_id, created_at, created_by, updated_at, updated_by)
-SELECT r.id, p.id, now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', now(), 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+SELECT r.id, p.id, now(), '019b938b-69b9-76c5-973b-547d442dbf98', now(), '019b938b-69b9-76c5-973b-547d442dbf98'
 FROM roles r, permissions p
 -- admin gets all permissions. user gets all user related permissions plus get and list operations on other resources
 WHERE r.name = 'admin' OR (r.name = 'user' AND p.resource = 'users' OR p.operation IN ('get', 'list'));
@@ -32,9 +32,9 @@ VALUES (
     (SELECT id FROM users WHERE username = 'adminuser'),
     (SELECT id FROM roles WHERE name = 'admin'),
     now(),
-    'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+    '019b938b-69b9-76c5-973b-547d442dbf98',
     now(),
-    'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+    '019b938b-69b9-76c5-973b-547d442dbf98'
 );
 
 -- assign user role to user 'exampleuser'
@@ -43,7 +43,7 @@ VALUES (
     (SELECT id FROM users WHERE username = 'exampleuser'),
     (SELECT id FROM roles WHERE name = 'user'),
     now(),
-    'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+    '019b938b-69b9-76c5-973b-547d442dbf98',
     now(),
-    'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+    '019b938b-69b9-76c5-973b-547d442dbf98'
 );
--- db/load_users.sql
+++ db/load_users.sql
@@ -5,6 +5,7 @@ INSERT INTO users (
     last_name,
     username,
     bio,
+    gender,
     profile_picture,
     location,
     password_hash,
@@ -13,5 +14,5 @@ INSERT INTO users (
     updated_at,
     updated_by
 ) VALUES
-    ('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'admin@fightpicker.com', 'Powerful', 'Admin', 'adminuser', 'Administrator account', 'https://example.com/images/admin.jpg', 'Headquarters', '$2a$05$nTpkCGxah9xFgVRFG89jh.3RqX6dbgs04E/p9QXLFExL1q6IpdjtK', '1990-01-01', '2024-01-01T12:00:00Z', '2024-01-01T12:00:00Z', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
-    ('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'user@fightpicker.com', 'Lowly', 'User', 'exampleuser', 'Example user account', 'https://example.com/images/user.jpg', 'Remote', '$2a$05$nTpkCGxah9xFgVRFG89jh.3RqX6dbgs04E/p9QXLFExL1q6IpdjtK', '1995-05-15', '2024-01-01T12:00:00Z', '2024-01-01T12:00:00Z', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb');
+    ('019b938b-69b9-76c5-973b-547d442dbf98', 'admin@fightpicker.com', 'Powerful', 'Admin', 'adminuser', 'Administrator account', 'other', 'http://localhost:8080/static/images/default-profile-picture-admin.webp', 'Headquarters', '$2a$05$nTpkCGxah9xFgVRFG89jh.3RqX6dbgs04E/p9QXLFExL1q6IpdjtK', '1990-01-01', '2024-01-01T12:00:00Z', '2024-01-01T12:00:00Z', '019b938b-69b9-76c5-973b-547d442dbf98'),
+    ('019b938c-227a-7c55-b48f-0d3fde1c1131', 'user@fightpicker.com', 'Lowly', 'User', 'exampleuser', 'Example user account', 'other', '', 'Remote', '$2a$05$nTpkCGxah9xFgVRFG89jh.3RqX6dbgs04E/p9QXLFExL1q6IpdjtK', '1995-05-15', '2024-01-01T12:00:00Z', '2024-01-01T12:00:00Z', '019b938c-227a-7c55-b48f-0d3fde1c1131');
